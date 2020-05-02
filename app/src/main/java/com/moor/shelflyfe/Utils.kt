86fdbaab33289148a0da4bd902a73ca30b7509e1
Marlon Moorer <marlonmoorer@outlook.com>
2020-05-02 15:45:15 -0400

    added dynamic colors for pages

---
+++ app/src/main/java/com/moor/shelflyfe/Utils.kt
@@ -0,0 +1,30 @@
+package com.moor.shelflyfe
+
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
+import java.lang.Exception
+import  androidx.palette.graphics.Palette
+import com.squareup.picasso.NetworkPolicy
+
+fun extractPallete(url:String,callback: (Exception?,Palette?) -> Unit){
+    Picasso.get().load(url).networkPolicy(NetworkPolicy.OFFLINE).into(object :Target{
+        override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+            callback(e,null)
+        }
+
+        override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+               callback(null,Palette.from(bitmap!!).generate())
+        }
+
+        override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+
+        }
+    })
+}
+
+fun preloadImage(url: String){
+    Picasso.get().load(url).fetch()
+}
--- app/src/main/java/com/moor/shelflyfe/ui/home/BestSellerAdapter.kt
+++ app/src/main/java/com/moor/shelflyfe/ui/home/BestSellerAdapter.kt
@@ -5,16 +5,33 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.moor.shelflyfe.api.nyt.models.Book
 import com.moor.shelflyfe.databinding.ItemBestSellerBinding
+import com.moor.shelflyfe.extractPallete
 import com.moor.shelflyfe.load
+import com.moor.shelflyfe.preloadImage
+import java.lang.Exception
 
 class BestSellerAdapter(val books: List<Book>): RecyclerView.Adapter<BestSellerAdapter.ViewHolder>() {
 
+    init {
+        books.forEach { book->
+            book.bookImage?.let { preloadImage(it) }
+        }
+    }
     class ViewHolder(val binding: ItemBestSellerBinding):RecyclerView.ViewHolder(binding.root){
 
         fun bind(book :Book)= binding.apply {
             bookTitle.text=book.title
-            book.bookImage?.let { coverImage.load(it) }
-            authorName.text =book.author
+            book.bookImage?.let {
+                coverImage.load(it)
+                extractPallete(it){ e,palette->
+                      palette?.vibrantSwatch?.let { swatch ->
+                          root.setBackgroundColor(swatch.rgb)
+                          bookTitle.setTextColor(swatch.titleTextColor)
+                          authorName.setTextColor(swatch.bodyTextColor)
+                      }
+                }
+            }
+            authorName.text = "By ${book.author}"
         }
     }
 
